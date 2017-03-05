1baecfe62f018c918d1b120443429f78642606e9
qrahm001 <qrahm001@ucr.edu>
2017-03-04 23:58:04 -0800

    Update exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 exit
-echo A && exit
-cd src || echo A && exit #exit should fail as that command is built into bash
-exit || ls -a && mkdir side
-echo Hello world; ls -a && mkdir secondshell; exit
+ls && exit
+kwdjkwd || exit
