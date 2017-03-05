dda927624d09af4803db0eb1e3964c77eef345b4
qrahm001 <qrahm001@ucr.edu>
2017-03-04 22:10:46 -0800

    Update exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,3 +1,7 @@
-src/a.out << EOF
-ls -a;echo Hello;ls -l&&cat hw2 #blah blah blah exit
-EOF
+#!/bin/sh
+
+exit
+echo A && exit
+cd src || echo A && exit #exit should fail as that command is built into bash
+exit || ls -a && mkdir side
+echo Hello world; ls -a && mkdir secondshell; exit
