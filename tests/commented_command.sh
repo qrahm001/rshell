b3510cedb1b7f04381b57c95909b141983d01391
qrahm001 <qrahm001@ucr.edu>
2017-03-04 22:10:29 -0800

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,3 +1,7 @@
-src/a.out << EOF
-ls -a;echo Hello;ls -l&&cat hw2 #blah balh blah
-EOF
+#!/bin/sh
+
+ls # -a
+mkdir test # && rmdir test
+echo A && echo B; #nothing is here
+execvp error #command should fail
+git status || git log; git checkout master # ls
