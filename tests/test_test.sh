3049717551cb8e4ada23aca4de80928c8afd295a
qrahm001 <qrahm001@ucr.edu>
2017-03-05 01:48:37 -0800

    Update test_test.sh

--- tests/test_test.sh
+++ tests/test_test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-test src
-test -e /src/3And.cpp && echo yes
-test -e tests
-test -f tests && echo no
-test -f tests/exit.sh
-test -d src
-[ src/2Command.h ] && echo yes
-[ -e /hello/no/hello ] && echo no
-[ -f src/2Command.h ]
-[ -d src ]
+test bin
+test -e bin
+test -e Makefile
+test -d bin
+test -d Makefile
+test -f bin
+test -f Makefile
+[ bin] && ls
+[ -d bin] || ls
+[ -f bin]; ls
