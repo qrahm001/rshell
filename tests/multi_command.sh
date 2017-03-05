c8cf58103f3eea820da309ece2a8834dcfd81e40
qrahm001 <qrahm001@ucr.edu>
2017-03-05 01:45:46 -0800

    Update multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-ls -a; echo hello; mkdir test
-ls -a; echo hello && mkdir test || echo world; git status
-echo A && echo B || echo C && echo D || echo E
-git log; git checkout exec || echo Error
-git branch; git status && git log || echo error; ls -a
-cd .. || echo Expected result
-mkdir test; cd test || rmdir test && echo cd unavailable
-echo Look at result ; echo Can end with semicolon ;
-ls -a; echo hello && mkdir test || echo world; git status
-echo Program ; echo is ; echo running ;
+ls -a; echo hello; mkdir one
+rm -r one || echo didnt work; ls
+ls && ls -a
+echo this || echo or that
